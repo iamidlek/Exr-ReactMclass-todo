e2908a0c006c9b0ed186c3eb279862fa49181069
iamidlek <iamidlek@naver.com>
2021-12-22 22:33:40 +0900

    todo with recoil done

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,5 +1,8 @@
 import { atom, selector } from "recoil";
 
+// 개발자를 도와주는 도구
+// "TO_DO"만 하면 0 으로 숫자로 관리됨
+// https://www.typescriptlang.org/ko/docs/handbook/enums.html
 export enum Categories {
   "TO_DO" = "TO_DO",
   "DOING" = "DOING",
@@ -23,7 +26,8 @@ export const toDoState = atom<IToDo[]>({
 });
 
 // selector atom의 output을 변경시킴
-// computed 같은 느낌
+// computed 같은 느낌 계산된 값 반환
+// 혹은 계산된 값들의 배열을 반환 (배열도 결국 값)
 export const toDoSelector = selector({
   key: "toDoSelector",
   get: ({ get }) => {
