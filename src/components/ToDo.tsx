6cf34bf7318a8a9a31da218a1a62da7c24703b19
iamidlek <iamidlek@naver.com>
2021-12-22 21:37:21 +0900

    recoil selector

--- src/components/ToDo.tsx
+++ src/components/ToDo.tsx
@@ -1,12 +1,54 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoState } from "../atoms";
 
-function ToDo({ text }: IToDo) {
+function ToDo({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoState);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+    // 카테고리 변경
+    setToDos((oldToDos) => {
+      const targetIndex = oldToDos.findIndex((toDo) => toDo.id === id);
+      const newToDo = { text, id, category: name as IToDo["category"] };
+      return [
+        ...oldToDos.slice(0, targetIndex),
+        newToDo,
+        ...oldToDos.slice(targetIndex + 1),
+      ];
+    });
+    // 다른 방법
+    // setToDos((prevTodos) => {
+    //   const targetIndex = prevTodos.findIndex((todo) => todo.id === id);
+    //   const newTodo: IToDo = {
+    //     id,
+    //     text,
+    //     category: name as IToDo["category"],
+    //   };
+    //   const newTodos = [...prevTodos]; // create new array
+    //   newTodos.splice(targetIndex, 1, newTodo);
+    //   return newTodos;
+    // });
+  };
   return (
     <li>
       <span>{text}</span>
-      <button>Doing</button>
-      <button>To Do</button>
-      <button>Done</button>
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          Doing
+        </button>
+      )}
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          To Do
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          Done
+        </button>
+      )}
     </li>
   );
 }
